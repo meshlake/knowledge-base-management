80de442cbee43c747179fa9881a5fff7c3f6cdcb
JavonYan <ycwen1119@163.com>
2023-08-02 15:11:17 +0800

    feat: 机器人创建、删除前端部分

---
+++ FrontEnd/src/components/TrimInput/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Input } from 'antd';
+import type { InputProps } from 'antd';
+
+const TrimInput: React.FC<InputProps> = (props) => {
+  const { onBlur, onChange } = props;
+  const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+    e.target.value = e.target.value?.trim();
+    onBlur?.(e);
+    onChange?.(e);
+  };
+
+  return <Input onBlur={handleBlur} {...props} />;
+};
+
+export default TrimInput;
