48ec30c4f37594d0c4fc15e46fed1af02a4e8f68
JavonYan <ycwen1119@163.com>
2023-08-15 11:49:47 +0800

    fix: 机器人绑定知识库方可用

--- FrontEnd/src/pages/Chatbot/Detail/components/Configuration/index.tsx
+++ FrontEnd/src/pages/Chatbot/Detail/components/Configuration/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { history } from '@umijs/max';
 import PromptConfig from '../PromptConfig';
 import KnowledgeConfig from '../KnowledgeConfig';
 import styles from './index.less';
+import { Button } from 'antd';
 
 type Props = {
   data?: Chatbot_API.Chatbot;
@@ -16,6 +18,21 @@ const Configuration: React.FC<Props> = (props) => {
       <div className={styles.title}>{data?.name}</div>
       <PromptConfig data={data} updateRequest={updateRequest} />
       <KnowledgeConfig data={data} updateRequest={updateRequest} />
+      {data?.knowledge_bases?.length ? (
+        <>
+          <div className={styles.buttonContainer}>
+            <Button
+              type="primary"
+              onClick={() => {
+                history.push(`/chat?botId=${data?.id}`);
+              }}
+              style={{ padding: '0 32px' }}
+            >
+              对话调试
+            </Button>
+          </div>
+        </>
+      ) : null}
     </div>
   );
 };
