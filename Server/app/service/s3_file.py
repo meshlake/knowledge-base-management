3812c670edcdb93fb75f90f2c5086bc3ca14281f
Fengnian Zhao <1947822757@qq.com>
2023-08-17 12:25:00 +0800

    fix: 🐛 修复审核后知识入库后未展示问题;优化知识展示;兼容无tagHeader文件;

--- Server/app/service/s3_file.py
+++ Server/app/service/s3_file.py
@@ -20,6 +20,7 @@ import pandas as pd
 
 NLTK_DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), "nltk_data")
 import nltk
+
 nltk.data.path = [NLTK_DATA_PATH] + nltk.data.path
 
 load_dotenv()
@@ -54,16 +55,6 @@ class S3FileLoader(BaseLoader):
                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
                 s3.download_file(self.bucket, self.key, file_path)
                 loader = UnstructuredFileLoader(file_path)
-                filetype = detect_filetype(file_path)
-                # if filetype == FileType.TXT:
-                #     elements = partition_text(
-                #         filename=file_path,
-                #         file=None,
-                #         encoding=None,
-                #         paragraph_grouper=None,
-                #     )
-                # else:
-                #     elements = loader.load()
                 elements = loader.load()
                 metadata = loader._get_metadata()
                 text = "\n\n".join([str(el) for el in elements])
@@ -94,22 +85,31 @@ class S3FileLoader(BaseLoader):
                 os.makedirs(os.path.dirname(file_path), exist_ok=True)
                 s3.download_file(self.bucket, self.key, file_path)
                 filetype = detect_filetype(file_path)
+                tag_headers = ["分类", "标签"]
 
                 elements = []
                 if (filetype == FileType.XLSX) or (filetype == FileType.XLS):
-                    elements = xlsx_loader(file_path, revision=1)
+                    elements = xlsx_loader(
+                        file_path, revision=1, tag_headers=tag_headers
+                    )
                 elif filetype == FileType.CSV:
-                    elements = csv_loader(file_path, revision=1)
+                    elements = csv_loader(
+                        file_path, revision=1, tag_headers=tag_headers
+                    )
 
                 documents = []
                 tags = []
                 for el in elements:
                     for data in el.data:
-                        if len(data.tags["分类"]) > 0 and len(data.tags["标签"]) > 0:
+                        if (
+                            data.tags
+                            and len(data.tags[tag_headers[0]]) > 0
+                            and len(data.tags[tag_headers[1]]) > 0
+                        ):
                             tags.append(
                                 {
-                                    "parentTag": data.tags["分类"][0],
-                                    "tag": data.tags["标签"][0],
+                                    "parentTag": data.tags[tag_headers[0]][0],
+                                    "tag": data.tags[tag_headers[1]][0],
                                 }
                             )
                 unique_tags = []
@@ -123,15 +123,21 @@ class S3FileLoader(BaseLoader):
                 )
                 for el in elements:
                     for data in el.data:
-                        if len(data.tags["分类"]) > 0 and len(data.tags["标签"]) > 0:
+                        if (
+                            data.tags
+                            and len(data.tags[tag_headers[0]]) > 0
+                            and len(data.tags[tag_headers[1]]) > 0
+                        ):
                             metadata = {
                                 "tag": [
                                     tag
                                     for tag in tag_entities
-                                    if tag.name == data.tags["标签"][0]
+                                    if tag.name == data.tags[tag_headers[1]][0]
                                 ][0].id
                             }
-                        text = "\n\n".join([str(el) for el in data.data])
+                        else:
+                            metadata = {}
+                        text = "\n\n".join([str(el[1]) for el in data.data])
                         documents.append(Document(page_content=text, metadata=metadata))
                 return documents
         except Exception as e:
